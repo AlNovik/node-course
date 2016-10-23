436e4073f89374e127a55a8a85ec70d9c0e1008f
Alexander Novik <alex.nvk88@gmail.com>
2016-10-24 01:51:03 +0300

    Registration by REST

--- koa-mongoose/public/js/reg.js
+++ koa-mongoose/public/js/reg.js
@@ -1,3 +1,18 @@
-/**
- * Created by alex on 10/23/16.
- */
+(function () {
+    $('[type=submit]').on('click', () => {
+        $.ajax({
+            method: 'POST',
+            url: '/api/user',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                displayName: $('input[name=name]').val(),
+                email: $('input[name=email]').val(),
+                password: $('input[name=password]').val()
+            })
+        }).then(resp => {
+            if (!resp.errors) {
+                window.location='/';
+            }
+        });
+    })
+})();
